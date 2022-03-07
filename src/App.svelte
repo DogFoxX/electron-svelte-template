2983a5001741158f4f7e8724014856f60b90fba1
DogFoxX <61309726+DogFoxX@users.noreply.github.com>
2022-03-08 03:10:08 +0800

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -6,8 +6,8 @@
 	<h1 class="heading">Thank you for choosing <br><span>electron-svelte-template</span></h1>
 	<img src="https://github.com/DogFoxX/assets/blob/master/electron-svelte-template/electron_svelte.png?raw=true" alt="Electron + Svelte Banner" width="100%" />
 	<h2 class="sub-heading">An ElectronJS Desktop App using the <span>{framework}</span> Framework</h2>
-	<p class="paragraph">Make sure to read through the <a href="https://github.com/DogFoxX/electron-svelte-template#readme">Readme</a> to properly get started</p>
-	<p class="paragraph">Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn more about <span>Svelte</span></p>
+	<p class="paragraph">Make sure to read through the <a href="https://github.com/DogFoxX/electron-svelte-template#readme" title="Shift + Click to open in new window&#013;https://github.com/DogFoxX/electron-svelte-template#readme">Readme</a> to properly get started</p>
+	<p class="paragraph">Visit the <a href="https://svelte.dev/tutorial" title="Shift + Click to open in new window&#013;https://svelte.dev/tutorial">Svelte tutorial</a> to learn more about <span>Svelte</span></p>
 </main>
 
 <style>
